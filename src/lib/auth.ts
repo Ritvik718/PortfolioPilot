a8418369b463fcf58bed8e9adbcffae631e7085b
Ritvik Ranjan <ritvikranjan22@gmail.com>
2025-08-03 10:36:32 +0000

    Error Authentication required.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,19 @@ import { auth } from '@/lib/firebase';
 import { onAuthStateChanged, type User } from 'firebase/auth';
 
 export function getCurrentUser(): Promise<User | null> {
-  return new Promise((resolve, reject) => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      unsubscribe();
-      resolve(user);
-    }, reject);
+  return new Promise((resolve) => {
+    if (auth.currentUser) {
+        return resolve(auth.currentUser);
+    }
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (user) => {
+        unsubscribe();
+        resolve(user);
+      },
+      () => {
+        resolve(null);
+      }
+    );
   });
 }
